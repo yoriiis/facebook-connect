6c21bed1fb42172bef65b95de64681ee5208b748
Joris Daniel <joris.daniel@gmail.com>
2014-10-10 17:24:04 +0200

    Update to 1.3
    Support last Facebook SDK
    New login method easier
    New name "_facebook" because Facebook already use "_FB"
    Init parameters use the same name as Facebook

--- js/facebook.js
+++ js/facebook.js
@@ -1,70 +1,134 @@
 /**
  *
  * Plugin:
- * @version 1.2
+ * @version 1.3
  *
  * @author: Joris DANIEL
  * @fileoverview: Easy way to use the Facebook API
  * Same support that Facebook API
  *
- * Copyright (c) 2013 Joris DANIEL
+ * Copyright (c) 2014 Joris DANIEL
  * Licensed under the MIT license
  *
-**/
+ **/
 
-(function ($, window) {
+(function($, window) {
 
     "use strict";
 
-    var _FB = {};
+    var _facebook = {};
 
-    _FB = {
+    _facebook = {
 
-        init: function( appID, what, scope ){
+        init: function(options, callback) {
 
-            var self                = this,
-            timerAutoGetData        = 500;
-            this.what               = what;
-            this.authorization      = scope;
-            this.doc                = $(document);
-            this.handlers           = {};
+            var self = this;
 
-            //Instanciate the Facebook apps with appID
-            FB.init({
-                appId               : appID,
-                status              : true,
-                cookie              : true,
-                xfbml               : true
-            });
+            this.options = $.extend({
+                appID: '',
+                locale: 'fr_FR',
+                async: true,
+                target: '/me',
+                authorization: '',
+                xfbml: true,
+                status: true,
+                version: 'v2.1',
+                debug: false
+            }, options);
 
-            //Prevent Facebook API not ready
-            setTimeout(function(){
+            this.$doc = $(document);
+            this.handlers = {};
+            this.onFBReady = callback;
+            this.status = null;
+            this.datas = null;
 
-                //If user already granted, auto get data
-                if( FB.getAuthResponse() ){
-                    self.status = 'connected';
-                    self.getData();
-                }
+            //On Facebook ready
+            window.fbAsyncInit = function() {
+                self.onFBAPIReady();
+            };
 
-            }, timerAutoGetData);
+            this.loadSDK();
 
         },
 
-        connect: function(){
+        onFBAPIReady: function() {
+
+            var self = this;
+
+            //Parse the DOM to instanciate Facebook plugins (Like)
+            if (this.options.xfbml) {
+                FB.XFBML.parse();
+            }
+
+            //AddClass in html tag when Facebook is ready
+            $('html').addClass('fb-ready');
+
+            FB.init({
+                appId: this.options.appID,
+                status: this.options.status,
+                xfbml: this.options.xfbml,
+                version: this.options.version
+            });
+
+            //OK everything is ready go on !
+            this.onFBReady();
+
+            //If status and user already granted, get data immediately
+            if (!this.options.status) return;
+
+            //Prevent Facebook API not ready
+            setTimeout(function() {
+                if (FB.getAuthResponse()) {
+                    self.status = 'connected';
+                    self.getDatas();
+                }
+            }, 500);
+
+        },
+
+
+        loadSDK: function() {
+
+            var self = this,
+                urlSDK = null;
+
+            //Choose SDK with debug or not
+            if (this.options.debug) {
+                urlSDK = '//connect.facebook.net/' + self.options.locale + '/sdk/debug.js';
+            } else {
+                urlSDK = '//connect.facebook.net/' + self.options.locale + '/sdk.js'
+            }
+
+            //Load the Facebook SDK JS and add the tag "fb-root"
+            (function(d, s, id) {
+                var js, tag = document.createElement('div');
+                if (d.getElementById(id)) return;
+                tag.id = 'fb-root';
+                js = d.createElement(s);
+                js.id = id;
+                js.async = self.options.async;
+                js.src = urlSDK;
+                d.getElementsByTagName('body')[0].appendChild(tag);
+                d.getElementsByTagName('body')[0].appendChild(js);
+            }(document, 'script', 'facebook-jssdk'));
+
+        },
+
+        connect: function() {
 
             var self = this;
 
             //Check status of Facebook connection
-            FB.getLoginStatus(function( response ) {
+            FB.getLoginStatus(function(response) {
 
                 //Status of the connection
                 self.status = response.status;
 
-                if( response.status === 'connected' ){
-                    self.getData();
-                }else if( response.status === 'not_authorized' ){
+                if (response.status === 'connected') {
+                    self.getDatas();
+                } else if (response.status === 'not_authorized') {
                     self.login();
-                }else{
+                } else {
                     self.login();
                 }
 
@@ -72,114 +136,68 @@
 
         },
 
-        login: function(){
+        login: function() {
 
             var self = this;
 
             //Facebook login function
-            FB.login(function( response ) {
+            FB.login(function(response) {
 
                 self.status = response.status;
 
-                if( response.authResponse ) {
-                    self.getData();
-                }else{
+                if (response.authResponse) {
+                    self.getDatas();
+                } else {
 
-                    if( response.status === 'not_authorized' ){
-                        self.trigger('not_authorized');
+                    if (response.status === 'not_authorized') {
+                        self.trigger('not_authorized', [self.status]);
                     }
 
                 }
 
-            }, { scope: self.authorization });
+            }, {
+                scope: this.options.authorization
+            });
 
         },
 
-        getData: function(){
+        getDatas: function() {
 
             var self = this;
 
             //Call Open Grap API
-            FB.api(self.what, function( response ) {
+            FB.api(this.options.target, function(response) {
 
-                //Save user data and status
-                self.data = response;
+                //Save user datas and status
+                self.datas = response;
 
                 //Trigger success event
-                self.trigger('connected');
+                self.trigger('connected', [self.datas, self.status]);
 
             });
 
         },
 
-        ready: function( locale, app ){
-
-            //On Facebook ready
-            window.fbAsyncInit = function() {
-
-                //Parse the DOM to instanciate Facebook plugins (Like)
-                FB.XFBML.parse();
-
-                //AddClass in html tag when Facebook is ready
-                $('html').addClass('fb-ready');
-
-                //Your code
-                app();
-
-            };
-
-            if( arguments.length == 2 ){
-                this.loadSDK( arguments[0] );
-            }else{
-                this.loadSDK();
-            }
-
-        },
-
-        on: function( eventName, method ){
-
+        on: function(eventName, method) {
             var self = this;
-            this.handlers[ method ] = function(){ method.call( self ) };
-            this.doc.on( eventName, this.handlers[ method ] );
-
+            this.handlers[method] = function() {
+                method.apply(self, arguments)
+            };
+            this.$doc.on(eventName, this.handlers[method]);
         },
 
-        off: function( eventName, method ){
-
-            this.doc.off( eventName, this.handlers[ method ] );
-
+        off: function(eventName, method) {
+            this.$doc.off(eventName, this.handlers[method]);
         },
 
-        trigger: function( eventName, datas ){
-
-            this.doc.trigger( eventName, ( typeof datas != 'undefined' ) ? datas : [] );
-
-        },
-
-        loadSDK: function( locale ){
-
-            var defaultLanguage = 'fr_FR',
-                async = true,
-                localeSDK = ( typeof locale != 'undefined' ) ? locale : defaultLanguage;
-
-            //Load the Facebook SDK JS and add the tag "fb-root"
-            (function(d, s, id){
-
-                var js, tag = document.createElement('div');
-                if (d.getElementById(id)) {return;}
-                tag.id = 'fb-root';
-                js = d.createElement(s); js.id = id; js.async = async;
-                js.src = "//connect.facebook.net/" + localeSDK + "/all.js";
-                d.getElementsByTagName('body')[0].appendChild( tag );
-                d.getElementsByTagName('body')[0].appendChild( js );
-
-            }(document, 'script', 'facebook-jssdk'));
+        trigger: function(eventName, datas) {
+            this.$doc.trigger(eventName, (typeof datas !== 'undefined') ? datas : []);
 
         }
 
     };
 
-    //Push _FB in window
-    window._FB = _FB;
+    //Push _facebook in window
+    window._facebook = _facebook;
 
-})( jQuery, window );
+})(jQuery, window);
